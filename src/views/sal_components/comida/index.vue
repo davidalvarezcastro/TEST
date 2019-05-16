75c3a4142797b70b5612b8ac47bcb625ee63bfbc
David Alvarez <davidac0291@gmail.com>
2019-05-17 01:43:48 +0200

    a

---
+++ src/views/sal_components/comida/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1> COMIDA </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'comida',
+  components: {
+    HelloWorld
+  }
+}
+</script>
